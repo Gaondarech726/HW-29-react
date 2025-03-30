7626db602bd7c6e127e1f3942173ca08dca2a50f
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-03-30 20:32:20 +0300

    fix root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { App } from 'App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { App } from './App';
 import './index.css';
 import { store } from './redux/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
